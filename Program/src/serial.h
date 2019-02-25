283df18e8dad3fd0201c8db03ac6123fc4baa8e0
valpiros <valpiros.programming@gmail.com>
2019-02-25 09:49:16 +0100

    changed repo, see README for details on changes

---
+++ Program/src/serial.h
@@ -0,0 +1,18 @@
+#ifndef SERIAL_H_
+#define SERIAL_H_
+
+#include <stdint.h>
+#include "stm32f4xx.h"
+#include "stm32f4_discovery.h"
+#include <serial.h>
+#include <type_others.h>
+#include <vt100.h>
+
+void serial_init(uint32_t baudrate);
+void serial_putchar(volatile char c);
+void serial_puts(volatile char *s);
+
+/* -1 if none */
+signed char serial_get_last_char(void);
+
+#endif /* SERIAL_H_ */
---
+++ Program/src/type_others.h
@@ -0,0 +1,68 @@
+/*
+ * type_others.h
+ *
+ *  Created on: 7 janv. 2019
+ *      Author: valen
+ */
+
+#include <stdint.h>
+#include <stm32f4xx.h>
+#include <stm32f4_discovery.h>
+#include <serial.h>
+#include <vt100.h>
+//#include "heap.h"
+
+#ifndef TYPE_OTHERS_H_
+#define TYPE_OTHERS_H_
+
+typedef unsigned char u_int8;
+typedef unsigned short u_int16;
+typedef unsigned long u_int32;
+typedef char int8;
+
+
+typedef struct {
+	u_int8 x;
+	u_int8 y;
+	u_int8 alive;
+	u_int8 lenght;
+	char skin[8];
+} pos;
+
+typedef struct{
+	u_int8 min_x;
+	u_int8 max_x;
+	u_int8 max_y;
+} extremum;
+
+extern pos ship;
+extern u_int8 ship_size;
+
+typedef enum {shoot_ennemy, shoot_ally} team;
+typedef enum {INIT, LOGO,TEST, START, GAME, END} state;
+typedef enum {right, left} direction;
+//typedef enum {loose, win} end_game;
+
+typedef struct {
+	u_int8 x;
+	u_int8 y;
+	team who;		// shoot_ennemy : 0 / shoot_ally : 1
+} shoot_pos;
+
+void move_ship (pos *ship, u_int8 ship_size);
+void serial_putship (void);
+void serial_shoot (shoot_pos *shoot_tab, u_int8 i);
+void border_init (void);
+void move_shoots (shoot_pos *shoot_tab);
+void delay (u_int32 a);
+void ennemy_type1 (pos *ennemy_tab, extremum *ennemy);
+void ennemy_type2 (pos *ennemy_tab, extremum *ennemy);
+void ennemy_shooting (pos *ennemy_tab, shoot_pos *shoot_tab);
+void hitbox (pos *ennemy_tab, shoot_pos *shoot_tab, pos *shi, u_int8 *lives);
+u_int8 Ps_RandomNumberGeneratory (void);
+void ally_shooting (u_int8 *cd_shoot, shoot_pos *shoot_tab);
+void ennemy_moving (pos *ennemy_tab, direction *fleet, extremum *ennemy);
+u_int8 ennemy_defeated (pos *ennemy_tab);
+void new_minmax (pos *ennemy_tab, extremum *ennemy);
+
+#endif /* TYPE_OTHERS_H_ */
